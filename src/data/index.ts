5a61f213afc8a0206ccef663733d3f9378416731
Ishmum <ishmum20000@gmail.com>
2024-10-01 02:06:04 +0800

    Add home carousel data

---
+++ src/data/index.ts
@@ -0,0 +1,20 @@
+export const homeCarouselItems = [
+  {
+    subtitle: 'DISCOVER',
+    subtitleColored: 'YOUR',
+    title: 'TALENTS',
+    imgUrl: "/images/home/home-hero-slide-1.jpeg",
+  },
+  {
+    subtitle: 'DISCOVER',
+    subtitleColored: 'YOUR',
+    title: 'TALENTS',
+    imgUrl: "/images/home/home-hero-slide-2.jpeg",
+  },
+  {
+    subtitle: 'DISCOVER',
+    subtitleColored: 'YOUR',
+    title: 'TALENTS',
+    imgUrl: "/images/home/home-hero-slide-3.png",
+  },
+]
