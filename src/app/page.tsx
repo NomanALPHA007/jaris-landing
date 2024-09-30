a0b51b206ed85e098b65c1007466d1bb8da4d577
Ishmum <ishmum20000@gmail.com>
2024-10-01 03:21:58 +0800

    Add discover section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { Hero } from "@/sections";
+import { Discover, Hero } from "@/sections";
 
 export default function Home() {
   return (
     <main className="" >
       <Hero />
+      <Discover />
     </main>
   );
 }
---
+++ src/sections/home/discover.tsx
@@ -0,0 +1,10 @@
+const Discover = () => {
+	return (
+		<div className=" h-screen max-md:h-[50vh] padding flex flex-col items-center justify-center text-jaris-blue font-[600]">
+			<p className="heading-md">Discover a community of</p>
+			<p className="heading-md underline-animated">unlimited possibility.</p>
+		</div>
+	);
+};
+
+export default Discover;
