143f5aeb69d486c952699c55be79d4699b71b460
Ishmum <ishmum20000@gmail.com>
2024-10-01 02:04:51 +0800

    Add animation

--- src/components/home-hero-slide/home-hero-slide.tsx
+++ src/components/home-hero-slide/home-hero-slide.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import { motion } from "framer-motion";
 
 const HomeHeroSlide = ({
 	subtitle,
@@ -17,13 +18,18 @@ const HomeHeroSlide = ({
 			style={{ backgroundImage: `url(${imgUrl})` }}
 		>
 			<div className="absolute inset-0 bg-jaris-blue opacity-75 " />
-			<div className="relative flex flex-col items-center justify-center pt-20 md:pt-40 lg:pt-60">
+			<motion.div
+				initial={{ opacity: 0, x: 40 }}
+				whileInView={{ opacity: 1, x: 0 }}
+				transition={{ duration: 0.2, ease: "easeInOut" }}
+				className="relative flex flex-col items-center justify-center pt-20 md:pt-40 lg:pt-60"
+			>
 				<div className="heading-secondary font-[500]">
 					<span className="text-white">{subtitle}</span>{" "}
 					<span className="text-jaris-gold">{subtitleColored}</span>
 				</div>
 				<div className="title font-[700] text-white">{title}</div>
-			</div>
+			</motion.div>
 		</div>
 	);
 };
