8875c21f438f5e23906fb764a0f8c3be1c510239
Ishmum <ishmum20000@gmail.com>
2024-10-01 22:15:02 +0800

    Add background

--- src/sections/home/discover.tsx
+++ src/sections/home/discover.tsx
@@ -1,6 +1,6 @@
 const Discover = () => {
 	return (
-		<div className=" h-screen max-md:h-[50vh] padding flex flex-col items-center justify-center text-jaris-blue font-[600]">
+		<div className=" h-screen max-md:h-[50vh] bg-bg-primary padding flex flex-col items-center justify-center text-jaris-blue font-[600]">
 			<p className="heading-md">Discover a community of</p>
 			<p className="heading-md underline-animated">unlimited possibility.</p>
 		</div>
