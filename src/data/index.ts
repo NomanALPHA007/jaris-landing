9cfc797889f820d9465c57201f114530d0121942
Ishmum <ishmum20000@gmail.com>
2024-10-02 22:13:22 +0800

    Add counts

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,54 +1,60 @@
 export const homeCarouselItems = [
-  {
-    subtitle: 'DISCOVER',
-    subtitleColored: 'YOUR',
-    title: 'TALENTS',
-    imgUrl: "/images/home/home-hero-slide-1.jpeg",
-  },
-  {
-    subtitle: 'DISCOVER',
-    subtitleColored: 'YOUR',
-    title: 'TALENTS',
-    imgUrl: "/images/home/home-hero-slide-2.jpeg",
-  },
-  {
-    subtitle: 'DISCOVER',
-    subtitleColored: 'YOUR',
-    title: 'TALENTS',
-    imgUrl: "/images/home/home-hero-slide-3.png",
-  },
+	{
+		subtitle: "DISCOVER",
+		subtitleColored: "YOUR",
+		title: "TALENTS",
+		imgUrl: "/images/home/home-hero-slide-1.jpeg",
+	},
+	{
+		subtitle: "DISCOVER",
+		subtitleColored: "YOUR",
+		title: "TALENTS",
+		imgUrl: "/images/home/home-hero-slide-2.jpeg",
+	},
+	{
+		subtitle: "DISCOVER",
+		subtitleColored: "YOUR",
+		title: "TALENTS",
+		imgUrl: "/images/home/home-hero-slide-3.png",
+	},
 ];
 
 export const homeExperiences = [
-  {
-    title: "20 Nationalities Represented",
-    subtitle: "Our campus proudly represents 20 nationalities, fostering a tapestry of cultures and viewpoints. This multicultural environment enriches education, nurtures global awareness and promotes unity among students.",
-  },
-  {
-    title: "90% School Attendance rate",
-    subtitle: "With a remarkable 90% attendance rate, our school empowers students for successful futures, Our education equips graduates with skills, knowledge and confidence to excel in higher education and beyond.",
-  },
+	{
+		title: "20 Nationalities Represented",
+		subtitle:
+			"Our campus proudly represents 20 nationalities, fostering a tapestry of cultures and viewpoints. This multicultural environment enriches education, nurtures global awareness and promotes unity among students.",
+	},
+	{
+		title: "90% School Attendance rate",
+		subtitle:
+			"With a remarkable 90% attendance rate, our school empowers students for successful futures, Our education equips graduates with skills, knowledge and confidence to excel in higher education and beyond.",
+	},
 ];
 
 export const homePartnerSlides = [
-  {
-    title: "Islamabad Board",
-    imgUrl: "/images/home/islamabad-board.png",
-  },
-  {
-    title: "Egypt Education Board",
-    imgUrl: "/images/home/egypt-ministry-education.png",
-  },
-  {
-    title: "Cambridge",
-    imgUrl: "/images/home/cambridge.png",
-  },
-  {
-    title: "College Board",
-    imgUrl: "/images/home/collegeboard.png",
-  },
-  {
-    title: "Sudan Education Board",
-    imgUrl: "/images/home/sudan-ministry-education.png",
-  },
-]
+	{
+		title: "Islamabad Board",
+		imgUrl: "/images/home/islamabad-board.png",
+	},
+	{
+		title: "Egypt Education Board",
+		imgUrl: "/images/home/egypt-ministry-education.png",
+	},
+	{
+		title: "Cambridge",
+		imgUrl: "/images/home/cambridge.png",
+	},
+	{
+		title: "College Board",
+		imgUrl: "/images/home/collegeboard.png",
+	},
+	{
+		title: "Sudan Education Board",
+		imgUrl: "/images/home/sudan-ministry-education.png",
+	},
+];
+
+export const studentCount = 750;
+export const teacherCount = 350;
+export const staffCount = 750;
