a474b44c074d60f3a95f8a72283be64ec0a9098a
Ishmum <ishmum20000@gmail.com>
2024-10-02 17:34:11 +0800

    Add experiences

--- src/data/index.ts
+++ src/data/index.ts
@@ -17,4 +17,15 @@ export const homeCarouselItems = [
     title: 'TALENTS',
     imgUrl: "/images/home/home-hero-slide-3.png",
   },
+];
+
+export const homeExperiences = [
+  {
+    title: "20 Nationalities Represented",
+    subtitle: "Our campus proudly represents 20 nationalities, fostering a tapestry of cultures and viewpoints. This multicultural environment enriches education, nurtures global awareness and promotes unity among students.",
+  },
+  {
+    title: "90% School Attendance rate",
+    subtitle: "With a remarkable 90% attendance rate, our school empowers students for successful futures, Our education equips graduates with skills, knowledge and confidence to excel in higher education and beyond.",
+  },
 ]
